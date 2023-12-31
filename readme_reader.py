4183c25e0c7319bc4812e74f098b465905994028
jasperan <23caj23@gmail.com>
2023-12-31 23:41:00 +0100

    feat: added rudimentary readme reader with http requests

---
+++ readme_reader.py
@@ -0,0 +1,28 @@
+import requests
+import re
+TAG_RE = re.compile(r'<[^>]+>')
+
+def remove_tags(text):
+    return TAG_RE.sub('', text)
+
+
+markdown_list = []
+with open('output.txt', 'r') as file:
+    for line in file:
+        line = line.rstrip('\n').strip()
+
+        markdown_url = '{}{}{}'.format(
+            'https://raw.githubusercontent.com',
+            line,
+            '/master/README.md'
+        ) 
+        markdown_list.append(markdown_url)
+
+
+print('Markdown List: {}'.format(markdown_list))
+for x in markdown_list:
+    response = requests.get(x)
+    print(response.text)
+
+
+
